1d8b53bfecf26df0ad671681dc39b2dd512688f8
Mustajab Ikram <gk9135@myamu.ac.in>
2022-10-17 22:45:25 +0530

    Recycler View Practice 2

--- app/src/main/java/com/drvoid/recyclerview/MainActivity.kt
+++ app/src/main/java/com/drvoid/recyclerview/MainActivity.kt
@@ -13,40 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var todos = mutableListOf(
-            Todo("task1", true),
-            Todo("task2", true),
-            Todo("task3", false),
-            Todo("task4", true),
-            Todo("task5", false),
-            Todo("task6", false),
-            Todo("task7", false),
-            Todo("task8", true),
-            Todo("task9", true),
+        var todoList = mutableListOf(
+            Todo("Task1", true),
+            Todo("Task2", true),
+            Todo("Task3", true),
+            Todo("Task4", false),
+            Todo("Task5", true),
+            Todo("Task6", false),
+            Todo("Task7", true),
+            Todo("Task8", false),
+            Todo("Task9", true)
         )
-        val adapter = TodoAdapter(todos)
-        val rvTodos = findViewById<RecyclerView>(R.id.rvTodos)
+        val adapter = TodoAdapter(todoList)
+        val rvTodos = findViewById<RecyclerView>(R.id.rvTodo)
         rvTodos.adapter = adapter
-        rvTodos.layoutManager = GridLayoutManager(this, 2)
-
-
-        val btnAdd = findViewById<Button>(R.id.btnAdd)
-        val etTodo = findViewById<EditText>(R.id.etTitle)
-
-        btnAdd.setOnClickListener {
-            Log.d("kk", "inside btn click event")
-
-            val title = etTodo.text.toString()
-            val todo = Todo(title, false)
-            if (etTodo.text == null) {
-                Log.d("kk", "inside empty title")
-                Toast.makeText(this, "Add something", Toast.LENGTH_LONG).show()
-            } else {
-                todos.add(todo)
-                etTodo.setText("")
-            }
-            adapter.notifyItemInserted(todos.size - 1)
-        }
-
+        rvTodos.layoutManager = LinearLayoutManager(this)
     }
 }
--- app/src/main/java/com/drvoid/recyclerview/TodoAdapter.kt
+++ app/src/main/java/com/drvoid/recyclerview/TodoAdapter.kt
@@ -7,24 +7,38 @@ import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
+var change = 3
 class TodoAdapter (
-    private val todos: List<Todo>
+    val todos: List<Todo>
         ) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
     inner class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
+         change--
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
+        if(change == 0) {
+            change = 3
+            view = LayoutInflater.from(parent.context).inflate(R.layout.item_not_do, parent, false)
+        }
         return TodoViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         holder.itemView.apply {
             val tvTitle = findViewById<TextView>(R.id.tvTitle)
-            tvTitle.text = todos[position].title
-
-            val isCheck = findViewById<CheckBox>(R.id.cbCheck)
-            isCheck.isChecked = todos[position].isChecked
+            val cbDone = findViewById<CheckBox>(R.id.cbDone)
+            val tvTitle2 = findViewById<TextView>(R.id.tvNotDo)
+            val cbDone2 = findViewById<CheckBox>(R.id.cbNotDone)
+            if(position % 3 == 0) {
+                tvTitle2.text = todos[position].title
+                cbDone2.isChecked = todos[position].isChecked
+            } else {
+                tvTitle.text = todos[position].title
+                cbDone.isChecked = todos[position].isChecked
+            }
         }
+
+
     }
 
     override fun getItemCount(): Int {
